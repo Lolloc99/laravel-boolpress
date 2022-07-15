34658f0aaa85dd78c948dc3207387d5a8f09be97
Lolloc99 <lolloc99@yahoo.it>
2022-07-15 18:13:58 +0200

    Api per show del post completata

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -11,8 +11,22 @@ class PostController extends Controller
     public function index() {
         $posts = Post::with(['category'])->paginate(6);
         return response()->json([
-            'success' => 'true',
+            'success' => true,
             'results' => $posts
+        ]);
+    }
+
+    public function show($slug) {
+        $post = Post::where('slug', '=', $slug)->with(['category', 'tags'])->first();
+        if ($post) {
+            return response()->json([
+                'success' => true,
+                'results' => $post
+            ]);
+        }
+        return response()->json([
+            'success' => false,
+            'error' => 'Nessun post trovato'
         ]);
     }
 }
