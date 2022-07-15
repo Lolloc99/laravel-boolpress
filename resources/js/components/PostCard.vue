2074b757771d4d9e15b40096adaa552d76142b30
Lolloc99 <lolloc99@yahoo.it>
2022-07-15 19:45:52 +0200

    Aggiunta pagina singolo post

--- resources/js/components/PostCard.vue
+++ resources/js/components/PostCard.vue
@@ -8,11 +8,9 @@
             {{ troncateText(post.content, 50) }}
         </p>
         </div>
-        <!-- 
         <div class="card-body">
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
-        </div> -->
+            <router-link :to="{ name: 'single-post', params: { slug: post.slug } }" class="card-link">Leggi post</router-link>
+        </div>
     </div>
 </template>
 
